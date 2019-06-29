abb6c194040c5c5f94123ffbc70600d138705171
Narzar21 <exp.two@yandex.ru>
2019-06-29 23:26:41 +0300

    SERVER react-backend

---
+++ bin/data.js
@@ -0,0 +1,32 @@
+module.exports=[
+  {
+    key: "0",
+    book: "book #0",
+    author: "author_0",
+    year: "1800-1854",
+  },
+  {
+    key: "1",
+    book: "book #1",
+    author: "author_1",
+    year: "1803-1885",
+  },
+  {
+    key: "2",
+    book: "book #2",
+    author: "author_2",
+    year: "1780-1822",
+  },
+  {
+    key: "3",
+    book: "book #3",
+    author: "author_3",
+    year: "1811-1857",
+  },
+  {
+    key: "4",
+    book: "book #4",
+    author: "author_4",
+    year: "1866-1904",
+  }
+]
---
+++ bin/www.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8888;
+var data = (require('d:/proj/react-backend/bin/data.js'))
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+app.get('/',function (request, response){
+  response.send('Привет от сервера!');
+})
+
+app.get('/tableData', function (request,response){
+  response.send(data)
+});
+
+console.log("сервер с портом "+port+" запущен");
+app.listen(port);
